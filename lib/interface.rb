e981bc31095e603026f4e431b25ceee1bb1898b0
Juan Garcia (he/him/his) <juan.garcia@fundingcircle.com>
2023-02-02 20:44:38 +0000

    WIP

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,29 +1,47 @@
 # 1. Print Welcome
+puts "--------------------"
+puts "Welcome to Instacart"
+puts "--------------------"
+
 # 2. Define your store (what items are for sale?)
-# 3. Get items from the user (shopping)
-# 3.a Print out your cart
+store = {
+  kiwi: { price: 1.25 },
+  banana: { price: 0.5 },
+  mango: { price: 4 },
+  asparagus: { price: 9 },
+}
+
+# Print the store
+puts "In our store today:"
+store.each do |item, value|
+  puts "#{item}: £#{value[:price]}"
+end
+
+item = ""
+cart = {}
+
+until item == "quit"
+  # 3. Get items from the user (shopping)
+  puts "Which item? (or 'quit' to checkout)"
+  item = gets.chomp
+  
+  if item != "quit"
+    if store.key?(item.to_sym)
+      if !cart.key?(item.to_sym)
+        cart[item.to_sym] = 0    
+      end
+      cart[item.to_sym] += 1
+    else 
+      puts "Sorry, we don't have #{item} today."
+    end
+  end
+end 
+
 # 4. Print the bill (checkout)
-
-
-
-# > --------------------
-# > Welcome to Instacart
-# > --------------------
-# > In our store today:
-# > kiwi: 1.25€
-# > banana: 0.5€
-# > mango: 4€
-# > asparagus: 9€
-# > --------------------
-# > Which item? (or 'quit' to checkout)
-# > kiwi
-# > Which item? (or 'quit' to checkout)
-# > pineapple
-# > Sorry, we don't have pineapple today.
-# > Which item? (or 'quit' to checkout)
-# > mango
-# > Which item? (or 'quit' to checkout)
-# > quit
-# > -------BILL---------
-# > TOTAL: 5.25€
-# > --------------------
+puts "-------BILL---------"
+total = 0
+cart.each do |item, amount|
+  total += amount * store[item][:price] 
+end
+puts "TOTAL: £#{total}"
+puts "--------------------"
