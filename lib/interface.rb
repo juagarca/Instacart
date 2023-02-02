2e3a2e5843b019bd83ab67ee27e44a8936e6ebae
Juan Garcia (he/him/his) <juan.garcia@fundingcircle.com>
2023-02-02 19:33:16 +0000

    WIP

---
+++ lib/interface.rb
@@ -0,0 +1,29 @@
+# 1. Print Welcome
+# 2. Define your store (what items are for sale?)
+# 3. Get items from the user (shopping)
+# 3.a Print out your cart
+# 4. Print the bill (checkout)
+
+
+
+# > --------------------
+# > Welcome to Instacart
+# > --------------------
+# > In our store today:
+# > kiwi: 1.25€
+# > banana: 0.5€
+# > mango: 4€
+# > asparagus: 9€
+# > --------------------
+# > Which item? (or 'quit' to checkout)
+# > kiwi
+# > Which item? (or 'quit' to checkout)
+# > pineapple
+# > Sorry, we don't have pineapple today.
+# > Which item? (or 'quit' to checkout)
+# > mango
+# > Which item? (or 'quit' to checkout)
+# > quit
+# > -------BILL---------
+# > TOTAL: 5.25€
+# > --------------------
