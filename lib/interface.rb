ce7c9bf21ea83fe0c0772cff45ba693682e0611c
Juan Garcia (he/him/his) <juan.garcia@fundingcircle.com>
2023-02-03 09:18:30 +0000

    WIP

--- lib/interface.rb
+++ lib/interface.rb
@@ -13,8 +13,8 @@ store = {
 
 # Print the store
 puts "In our store today:"
-store.each do |item, value|
-  puts "#{item}: £#{value[:price]}"
+store.each do |item, price_and_stock|
+  puts "#{item}: £#{price_and_stock[:price]}"
 end
 
 item = ""
@@ -27,17 +27,23 @@ until item == "quit"
   
   if item != "quit"
     if store.key?(item.to_sym)
+      # If the store has the item, we ask for how many
       puts "How many?"
       quantity = gets.chomp.to_i
+        # If the store has enough stock for the item
         if store[item.to_sym][:stock] >= quantity
+          # If the cart does not have the item yet we need to initialize the quantity to 0 first
           if !cart.key?(item.to_sym)
             cart[item.to_sym] = 0    
           end
+          # We sum the quantity chosen to the previous item quantity
           cart[item.to_sym] += quantity
           store[item.to_sym][:stock] -= quantity
+        # If the store doesn't have enough stock for the item
         else
           puts "Sorry, there are only #{store[item.to_sym][:stock]} #{item}(s) left."
         end
+      # If the store doesn't have the item in stock
       else 
       puts "Sorry, we don't have #{item} today."
     end
@@ -47,10 +53,12 @@ end
 # 4. Print the bill (checkout)
 puts "-------BILL---------"
 bill = 0
-cart.each do |item, amount|
-  total = amount * store[item][:price] 
+cart.each do |item, quantity|
+  total = quantity * store[item][:price] 
   bill += total
-  puts "#{item}: #{amount} x £#{store[item.to_sym][:price]} = £#{total}"
+  # Prints every individual item in the cart with its quantity
+  puts "#{item}: #{quantity} x £#{store[item.to_sym][:price]} = £#{total}"
 end
+# Prints the total bill
 puts "TOTAL: £#{bill}"
 puts "--------------------"
